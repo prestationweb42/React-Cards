73ab87a29b73cb5affc7c08a8fae467daba16a93
danivent23122020 <dani.vent@orange.fr>
2022-10-31 16:11:51 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
