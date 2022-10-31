638060d42f0eaac0201008a42bbe567bc5a97b1e
danivent23122020 <dani.vent@orange.fr>
2022-10-31 20:19:56 +0100

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,35 +7,37 @@ import "../Styles/Navbar.css";
 function Naviagation() {
     return (
         <>
-            {/* <Navbar bg="primary" variant="dark"> */}
-            <Navbar className="nav-bar">
+            <Navbar className="nav-bar" expand="xl">
                 <Container>
                     <Navbar.Brand as={Link} to="/" className="nav-brand">
                         Arbre à chat
                     </Navbar.Brand>
-                    <Nav className="ms-auto">
-                        <Nav.Link
-                            as={Link}
-                            to="/guideachat"
-                            className="nav-link fs-4"
-                        >
-                            Guide d'achat
-                        </Nav.Link>
-                        <Nav.Link
-                            as={Link}
-                            to="/promo"
-                            className="nav-link fs-4"
-                        >
-                            Promo
-                        </Nav.Link>
-                        <Nav.Link
-                            as={Link}
-                            to="/connaissezvous"
-                            className="nav-link fs-4"
-                        >
-                            Connaissez-vous?
-                        </Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link
+                                as={Link}
+                                to="/guideachat"
+                                className="nav-link fs-4"
+                            >
+                                Guide d'achat
+                            </Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/promo"
+                                className="nav-link fs-4"
+                            >
+                                Promo
+                            </Nav.Link>
+                            <Nav.Link
+                                as={Link}
+                                to="/connaissezvous"
+                                className="nav-link fs-4"
+                            >
+                                Connaissez-vous?
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
