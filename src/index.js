79fd91d17be9b9ad601f61015cddce1ecc3bae0e
danivent23122020 <dani.vent@orange.fr>
2022-11-10 19:36:24 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import "./Styles/index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+App.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+App.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
