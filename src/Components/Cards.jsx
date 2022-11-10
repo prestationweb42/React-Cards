0199121382975d61241c8561eac88a7dd03531a5
danivent23122020 <dani.vent@orange.fr>
2022-11-10 07:50:59 +0100

    commit

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -27,11 +27,11 @@ const Cards = (props) => {
                         <p className="card-text text-center">{props.desc}</p>
                         <p className="card-text text-center">{props.desc}</p>
 
-                        <button className="btn btn-outline-success mt-3 p-2">
+                        <a className="btn btn-outline-success mt-3 p-2">
                             <img src={logoamazon} alt="logo amazon" />
                             <span>Voir l'offre</span>
                             <i className="bx bx-chevron-right"></i>
-                        </button>
+                        </a>
                     </div>
                 </div>
             </div>
